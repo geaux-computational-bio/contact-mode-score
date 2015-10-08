93e2ced31d98be813b3a26593a72fcd630156fb2
EricTing <yunding.whu@gmail.com>
2015-10-08 16:57:08 -0500

    add more examples
    	new file:   data/2ackA.pdb
    	new file:   data/2ackA1.sdf
    	new file:   data/2ackA1_new_6.sdf
    	modified:   data/cms.sh

--- data/cms.sh
+++ data/cms.sh
@@ -5,6 +5,9 @@ bin=../bin/cms
 echo "between different conformations"
 $bin -f -r -c --lig1 ./1a07C1.sdf --lig2 ./1a07C1_.sdf --prt1 ./1a07C.pdb --prt2 ./1a07C_.pdb
 
+echo "between different conformations"
+$bin -frc --lig1 ./2ackA1.sdf --lig2 ./2ackA1_new_6.sdf --prt1 ./2ackA.pdb --prt2 ./2ackA.pdb 
+
 echo 
 
 echo "for identical complexes"
