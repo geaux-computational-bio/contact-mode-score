dbcb7c16f980244aea91e9ef8e22ed1b49c2a9a2
EricTing <yunding.whu@gmail.com>
2015-11-10 10:02:23 -0600

    fix typo

--- data/cms.sh
+++ data/cms.sh
@@ -8,7 +8,5 @@ $bin -f -r -c --lig1 ./1a07C1.sdf --lig2 ./1a07C1_.sdf --prt1 ./1a07C.pdb --prt2
 echo "between different conformations"
 $bin -frc --lig1 ./2ackA1.sdf --lig2 ./2ackA1_new_6.sdf --prt1 ./2ackA.pdb --prt2 ./2ackA.pdb 
 
-echo 
-
 echo "for identical complexes"
 $bin -frc --lig1 ./1a07C1.sdf --lig2 ./1a07C1.sdf --prt1 ./1a07C.pdb --prt2 ./1a07C.pdb
